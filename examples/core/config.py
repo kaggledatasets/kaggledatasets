425ed7eb48beea1b62a84c4a2e10763df78b54e4
Omkar Prabhu <prabhuomkar@pm.me>
2019-11-16 19:18:54 +0530

    Updated setup.py and Added examples

--- examples/core/config.py
+++ examples/core/config.py
@@ -12,4 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Kaggle Datasets Examples"""
+"""Kaggle Datasets Core Examples"""
+
+from kaggledatasets.core import Config
+
+
+cfg = Config()
+print(cfg.get_credentials())
+print(cfg.get_username())
+print(cfg.get_token())
--- setup.py
+++ setup.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 from setuptools import setup, find_packages
 
 
-version = '0.1.0'
-package_name = 'kaggledatasets'
+version = "0.1.0"
+sha = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8") 
+package_name = "kaggledatasets"
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 def write_version_file():
-    version_path = os.path.join(cwd, package_name, 'version.py')
-    with open(version_path, 'w') as f:
+    version_path = os.path.join(cwd, package_name, "version.py")
+    with open(version_path, "w") as f:
         f.write("__version__ = '{}'\n".format(version))
         f.write("git_version = {}\n".format(repr(sha)))
 
 write_version_file()
 
-readme = open('README.md').read()
+readme = open("README.md").read()
 
-requirements = open('requirements.txt').read().split()
+requirements = open("requirements.txt").read().split()
 
 setup(
     name=package_name,
     version=version,
-    author='Omkar Prabhu',
-    author_email='prabhuomkar@yandex.com',
-    url='https://github.com/kaggledatasets/kaggledatasets',
-    description='Collection of Kaggle Datasets ready to use for Everyone',
+    author="Omkar Prabhu",
+    author_email="prabhuomkar@yandex.com",
+    url="https://github.com/kaggledatasets/kaggledatasets",
+    description="Collection of Kaggle Datasets ready to use for Everyone",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    license='Apache License 2.0',
-    packages=find_packages(exclude=('tests',)),
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    packages=find_packages(exclude=("tests",)),
     zip_safe=True,
     install_requires=requirements,
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
 )
