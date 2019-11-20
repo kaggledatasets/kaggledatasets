a78920aaf8c0791892683664d0fe505d0fd9443e
Omkar Prabhu <prabhuomkar@pm.me>
2019-11-20 22:34:50 +0530

    Updated to Conda for Python 3+

--- scripts/deploy_anaconda.sh
+++ scripts/deploy_anaconda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/kaggledatasets-*.tar.bz2 --output-dir conda-bld/
+conda convert --platform all $HOME/miniconda3/conda-bld/linux-64/kaggledatasets-*.tar.bz2 --output-dir conda-bld/
 anaconda -t $ANACONDA_API_TOKEN upload --user kaggledatasets conda-bld/**/kaggledatasets-*.tar.bz2
 echo "Successfully deployed to Anaconda.org."
 exit 0
