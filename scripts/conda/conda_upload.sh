5d9ed6b86f22b598a5fc5b99571819c9183205ce
Omkar Prabhu <prabhuomkar@pm.me>
2019-11-20 23:57:10 +0530

    Script fixes for Conda Deployments

--- scripts/conda/conda_upload.sh
+++ scripts/conda/conda_upload.sh
@@ -5,6 +5,6 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`0.0.1`
+export VERSION="0.0.1"
 conda build .
-anaconda -t $ANACONDA_API_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
+anaconda -t $ANACONDA_API_TOKEN upload --user $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
