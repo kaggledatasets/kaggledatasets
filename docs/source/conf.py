8fa02b9889659e023e299e8c626ec049f31cb666
Omkar Prabhu <prabhuomkar@pm.me>
2019-11-20 14:20:50 +0530

    Fixes for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Error building on readthedocs website
+# https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
